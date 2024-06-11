77d6777abf1ef444ec32f072775319dac61c565c
0bese <134105909+0bese@users.noreply.github.com>
2024-06-11 01:24:41 -0400

    completed the backend

--- Assignment 3/server.js
+++ Assignment 3/server.js
@@ -13,56 +13,69 @@ const express = require("express");
 const legoData = require("./modules/legoSets");
 const app = express();
 const path = require("path");
+const { log } = require("console");
 
 const API_PORT = process.env.PORT || 8000;
 
 app.use(express.static(path.join(__dirname, "public")));
 //route
 app.get("/", async (req, res) => {
-  await res.sendFile(__dirname, "views", "home.html");
+  await res.sendFile(path.join(__dirname, "views", "/home.html"));
+});
+
+app.get("/about", async (req, res) => {
+  await res.sendFile(path.join(__dirname, "views", "about.html"));
 });
 
 //GET LEGO SETS
 app.get("/lego/sets", async (req, res) => {
-  try {
-    const sets = await legoData.getAllSets();
-    res.json(sets);
-  } catch (error) {
-    res.status(500).send(error);
+  const theme = req.query.theme;
+
+  if (theme) {
+    legoData.initialize().then(() => {
+      legoData
+        .getSetsByTheme(theme)
+        .then((sets) => {
+          res.send(sets);
+        })
+        .catch((err) => {
+          res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+        });
+    });
+  } else {
+    legoData.initialize().then(() => {
+      legoData.getAllSets().then((sets) => {
+        res.send(sets);
+      });
+    });
   }
 });
 
 //GET LEGO SETS NUM-DEMO
-app.get("/lego/sets/num-demo", async (req, res) => {
-  const demoNum = "03093-1";
-  try {
-    const sets = await legoData.getSetByNum(demoNum);
-    res.json(sets);
-  } catch (error) {
-    res.status(400).send(error);
-  }
+app.get("/lego/sets/:num-demo", async (req, res) => {
+  legoData.initialize().then(() => {
+    legoData
+      .getSetByNum(req.params.set_num)
+      .then((sets) => {
+        res.send(sets);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+  });
 });
 
-//GET LEGO SETS THEME-DEMO
-app.get("/lego/sets/theme-demo", async (req, res) => {
-  const demoTheme = "town";
+// //GET LEGO SETS THEME-DEMO
+// app.get("/lego/sets/theme-demo", async (req, res) => {
+//   const demoTheme = "town";
 
-  try {
-    const set = await legoData.getSetsByTheme(demoTheme);
-    res.json(set);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-//ABOUT
-app.get("/about", async (req, res) => {
-  try {
-    res.render("./views/about.html");
-  } catch (error) {
-    res.status(501).send(error);
-  }
-});
+//   try {
+//     const set = await legoData.getSetsByTheme(demoTheme);
+//     res.json(set);
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
 
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
